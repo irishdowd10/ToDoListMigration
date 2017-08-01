1662c1232f3cc3b6a7b2981393591d8d566fc192
John Dowd <irishdowd10@gmail.com>
2017-08-01 15:20:54 -0700

    Set up files categories and items and db

---
+++ ToDoListMigration/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListMigration.Models
+{
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+		public virtual ICollection<Item> Items { get; set; }
+    }
+}
